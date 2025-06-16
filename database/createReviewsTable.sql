74c57008a69dfb31fbd05a47ef398d797a64f5cf
Aviv <aviv.dror@gmail.com>
2025-06-17 01:07:25 +0200

    week-2 reservations crud setup

--- database/createReviewsTable.sql
+++ database/createReviewsTable.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Reviews (
 Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-Title varchar(200) UNIQUE,
+Title varchar(200) UNIQUE NOT NULL,
 [Description] text(500),
-Stars int,
+Stars int NOT NULL,
 CreatedDate date,
 MealId INTEGER,
 FOREIGN KEY (MealId) REFERENCES Meals (MealId)
